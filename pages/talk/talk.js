e87f2fab5065c45a0e673e1d680df1674f6809f3
roselle <chensjpro@163.com>
2017-05-08 11:09:14 +0800

    准备新增聊天功能，引导内容

--- pages/talk/talk.js
+++ pages/talk/talk.js
@@ -1,6 +1,10 @@
 // pages/talk/talk.js
 Page({
-  data:{},
+  data:{
+    face:'(=･ω･=)',
+    message:'还没做~',
+    clock:''
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
   },
@@ -8,7 +12,37 @@ Page({
     // 页面渲染完成
   },
   onShow:function(){
-    // 页面显示
+    var that = this;
+    var timeout = setTimeout(function(){
+      that.setData({
+        face:'╮(╯▽╰)╭',
+        message:'真的没做啦'
+      })
+    },2000)
+    var timeout = setTimeout(function(){
+      that.setData({
+        face:'(╯-_-)╯~╩╩',
+        message:'这么说你是不信咯'
+      })
+    },5000)
+    var timeout = setTimeout(function(){
+      that.setData({
+        face:'(*ﾟДﾟ*) ',
+        message:'还不点后退？！'
+      })
+    },8000)
+    var timeout = setTimeout(function(){
+      that.setData({
+        face:'_(:з」∠)_',
+        message:'好吧 看在你这么诚心的份上'
+      })
+    },110000)
+    var timeout = setTimeout(function(){
+      that.setData({
+        face:'(థ౪థ)',
+        message:'我看你能不能等这么久'
+      })
+    },12000)
   },
   onHide:function(){
     // 页面隐藏
