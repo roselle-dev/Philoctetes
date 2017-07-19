b5c896dc3e918dfd3590ba92e1e85058ea1716dc
roselle <chensjpro@163.com>
2017-07-19 16:09:11 +0800

    修改逻辑

--- pages/sudoku/index.js
+++ pages/sudoku/index.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+
   },
 
   /**
@@ -21,55 +21,55 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   },
 
-  check:function(event){
+  check: function (event) {
     var value = event.detail.value;
     console.log(value);
-    if (value != 1 || value != 2 || value != 3 || value != 4 || value != 5 || value != 6 || value != 7 || value != 8 || value != 9){
+    if (value != '1' && value != '2' && value != '3' && value != '4' && value != '5' && value != '6' && value != '7' && value != '8' && value != '9') {
       wx.showToast({
         title: '请输入1-9数字',
         image: '../../images/icons/alarm.png',
