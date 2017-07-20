9512b8e7fda2a104904e6fea225cf00f1176c29e
roselle <chensjpro@163.com>
2017-07-20 16:21:40 +0800

    完成调试。能够成功计算数独

--- pages/sudoku/index.js
+++ pages/sudoku/index.js
@@ -65,6 +65,16 @@ Page({
   onShareAppMessage: function () {
 
   },
+  example: function () {
+    var that = this;
+    var problem = [9, 1,"", 4,"" ,"", "", "", 3,"", 2, 7, "", "", "", "", "", "", "", "", 3, "", 1, "", "", 6, 8, 7, "", "", "", 4, 3,
+      6, 1, 9, 3, 9, 5, 8, 6, 1, 4, 2, 7, 1, 6, 4, 2, 7, "", "", "", 5, 5, 4, "", "", 9, "", 3, "", "", "", "", "", "", "",
+      "", 1, 5, "", 8, "", "", "", "", 4, "", 9, 2];
+    that.setData({
+      p: problem
+    })
+
+  },
   /**
    * 输入检查
    */
@@ -74,7 +84,7 @@ Page({
     var value = event.detail.value;
     var id = event.currentTarget.id;
     problem[Number(id)] = value;
-    if (value != '1' && value != '2' && value != '3' && value != '4' && value != '5' && value != '6' && value != '7' && value != '8' && value != '9') {
+    if (value != '1' && value != '2' && value != '3' && value != '4' && value != '5' && value != '6' && value != '7' && value != '8' && value != '9' && value != '') {
       wx.showToast({
         title: '请输入1-9数字',
         image: '../../images/icons/alarm.png',
@@ -92,6 +102,7 @@ Page({
    * 生成答案
    */
   solve: function () {
+    console.log("调用solve方法!");
     var that = this;
     var problem = that.data.p;
     for (var i = 0; i < problem.length; i++) { //将输入数据转化，未填写的添0；填写的转为数字
@@ -111,6 +122,7 @@ Page({
           duration: 2000
         });
       }
+      console.log("初始化问题成功");
       for (var i = 0; i <= 81 * 4; i++) {
         S[i] = 0;
         L[i] = i - 1;
@@ -137,7 +149,7 @@ Page({
             this.Ins_node((i - 1) * 9 + j, cnt + 1);// （c,cnt），第c格有一个数字
             this.Ins_node(81 + (i - 1) * 9 + k, cnt + 2);// 第x行有y，如82，则91-81=10，第2行有1
             this.Ins_node(81 * 2 + (j - 1) * 9 + k, cnt + 3);// 第x列有y，如172,172-162=10，第2列有1
-            this.Ins_node(81 * 3 + (subgrid(i, j) - 1) * 9 + k, cnt + 4);// 第x宫有y。
+            this.Ins_node(81 * 3 + (this.subgrid(i, j) - 1) * 9 + k, cnt + 4);// 第x宫有y。
             cnt += 4;
           } else {
             for (var k = 1; k <= 9; k++) {
@@ -152,28 +164,52 @@ Page({
               this.Ins_node((i - 1) * 9 + j, cnt + 1);
               this.Ins_node(81 + (i - 1) * 9 + k, cnt + 2);
               this.Ins_node(81 * 2 + (j - 1) * 9 + k, cnt + 3);
-              this.Ins_node(81 * 3 + (subgrid(i, j) - 1) * 9 + k, cnt + 4);
+              this.Ins_node(81 * 3 + (this.subgrid(i, j) - 1) * 9 + k, cnt + 4);
               cnt += 4;
             }
           }
         }
       }
+      console.log("创建链表成功");
       var k = 0;
       for (var i = 1; i <= 9; i++) {
         for (var j = 1; j <= 9; j++) {
+          console.log("map[i][j]:"+map[i][j]+"-k:"+k+"-i:"+i+"-j:"+j);
           if (map[i][j] != 0) {
             k++;
             this.Remove(nCol[head[i][j][map[i][j]]]);
             for (var u = R[head[i][j][map[i][j]]]; u != head[i][j][map[i][j]]; u = R[u]) {
+              console.log("u:" + u + "-head:" + head[i][j][map[i][j]]);
               this.Remove(nCol[u]);
             }
           }
         }
       }
-      var flag = dfs(k + 1);
+      var flag = this.dfs(k + 1);
       if (flag) {
         console.log("solved!");
-        console.log(map);
+        var index=0;
+        for(var i=1;i<10;i++){
+          for(var j=1;j<10;j++){
+            problem[index]=map[i][j];
+            index++;
+          }
+        }
+        that.setData({
+          p: problem
+        })
+        wx.showToast({
+          title: '答案出来啦！',
+          icon: 'success',
+          duration: 2000
+        })
+        break;
+      }else{
+        wx.showToast({
+          title: '你肯定写错了！',
+          image: '../../images/icons/alarm.png',
+          duration: 2000
+        });
         break;
       }
     }
@@ -182,6 +218,7 @@ Page({
    * 初始化
    */
   init: function (p) {
+    console.log("调用init方法");
     if (p.length != 81) {
       return false;
     } else {
@@ -202,7 +239,10 @@ Page({
 	 * @return
 	 */
   subgrid: function (i, j) {
-    return (i / 3) * 3 + (j / 3 + 1);
+    i--;
+    j--;
+    var g = (parseInt(i / 3)) * 3 + (parseInt(j / 3) + 1);
+    return g;
   },
   /**
    * 加入链表
@@ -221,6 +261,7 @@ Page({
    * 选中节点后的移除操作（移除所在行，及所在行元素列重复的行）
    */
   Remove: function (c) {
+    console.log("remove"+c);
     L[R[c]] = L[c];
     R[L[c]] = R[c];
     for (var i = D[c]; i != c; i = D[i]) {
@@ -235,6 +276,7 @@ Page({
    * 恢复节点（因它删除的节点也一并恢复）
    */
   resume: function (c) {
+    console.log("resume:"+c);
     for (var i = U[c]; i != c; i = U[i]) {
       for (var j = L[i]; j != i; j = L[j]) {
         U[D[j]] = D[U[j]] = j;
@@ -247,6 +289,12 @@ Page({
    * 核心方法 dancing links
    */
   dfs: function (k) {
+    if(k==breakflag){
+      return false;
+    }else{
+      breakflag=k;
+    }
+    console.log("dfs"+k);
     if (k > 81) {
       return true;
     }
@@ -261,10 +309,10 @@ Page({
         c = i;
       }
     }
-    Remove(c);
+    this.Remove(c);
     for (var i = D[c]; i != c; i = D[i]) {
       var temp = nRow[i];
-      map[temp / 100][(temp / 10) % 10] = temp % 10;
+      map[parseInt(temp / 100)][(parseInt(temp / 10)) % 10] = temp % 10;
       for (var j = R[i]; j != i; j = R[j]) {
         this.Remove(nCol[j]);
       }
@@ -302,3 +350,4 @@ var map = new Array(10);
 for (var i = 0; i < 10; i++) {
   map[i] = new Array(10);
 }
+var breakflag = 999999;
