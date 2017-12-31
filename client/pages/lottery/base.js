50fd92303a89bbb41266d1b7f53d6227ce6cc077
roselle <chensjpro@163.com>
2017-12-31 16:30:11 +0800

    数字滚动解决，下一步把数据数组话 可配置化

---
+++ client/pages/lottery/base.js
@@ -0,0 +1,75 @@
+// pages/lottery/base.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    number1:"1234567890",
+    margin1:0
+  },
+  doIt:function(){
+    var oneTurn = "1234567890";
+    var number1 = this.data.number1;
+    var that = this;
+    that.setData({
+      number1: number1 + oneTurn + oneTurn,
+      margin1: -180 + (-450)
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
