c22a9f4f26b47b3a3bd132dddd3af9a526e6b3d8
roselle <chensjpro@163.com>
2018-05-03 17:38:27 +0800

    改为黑色主题

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -36,14 +36,6 @@ Page({
       title: '首页'
     })
     var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      console.log(userInfo);
-      that.setData({
-        userInfo: userInfo
-      })
-    })
     wx.request({
       url: config.service.host + "/weapp/everyday",
       success: function (result) {
