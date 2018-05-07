76d05cccd5a2e2f93f5758fd6fc645f1912bc605
roselle <chensjpro@163.com>
2018-05-07 17:25:49 +0800

    弹幕设置

--- client/pages/barrage/barrage.js
+++ client/pages/barrage/barrage.js
@@ -2,6 +2,10 @@
 Page({
   data: {
     menuBottom:0,
+    textColor:'#FFF',
+    textSize:24,
+    text:'点击文字设置您的弹幕',
+    bottom:-1
   },
 
   onLoad: function (options) {
@@ -45,5 +49,59 @@ Page({
     that.setData({
       menuBottom: value
     })
+  },
+  textIn:function(e){
+    var that = this;
+    that.setData({
+      text: e.detail.value
+    })
+  },
+  colorIn:function(e){
+    var that = this;
+    var reg = /^([0-9a-fA-F]{6}|[0-9a-fA-F]{3})$/;
+    if(reg.test(e.detail.value)){//符合颜色的表达式
+      that.setData({
+        textColor: "#"+e.detail.value
+      })
+    }
+  },
+  sizeChange:function(e){
+    var that = this;
+    that.setData({
+      textSize: e.detail.value
+    })
+  },
+  colorOne:function(){
+    var that = this;
+    that.setData({
+      textColor: "#FF6666"
+    })
+  },
+  colorTwo: function () {
+    var that = this;
+    that.setData({
+      textColor: "#FFFF66"
+    })
+  },
+  colorThree: function () {
+    var that = this;
+    that.setData({
+      textColor: "#99CCFF"
+    })
+  },
+  colorFour: function () {
+    var that = this;
+    that.setData({
+      textColor: "#99CC33"
+    })
+  },
+  colorFive: function () {
+    var that = this;
+    that.setData({
+      textColor: "#FF33CC"
+    })
+  },
+  start:function(){
+    this.interval = setInterval()
   }
 })
