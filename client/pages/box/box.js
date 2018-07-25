5432baa8c05abced310eca5b194aad794e213df8
roselle <chensjpro@163.com>
2018-07-25 17:40:49 +0800

    修改图表

--- client/pages/box/box.js
+++ client/pages/box/box.js
@@ -2,6 +2,8 @@
 import * as echarts from '../../ec-canvas/echarts';
 const app = getApp();
 const RANKLIST_URL = 'https://api.shenjian.io/?appid=29cfcdcb5717cd582f75797c2a10fbe6';
+//15种颜色
+const COLORS = ['#FD6585', '#ABDCFF', '#FEB692', '#CE9FFC', '#32CCBC', '#F6416C', '#81FBB8', '#E2B0FF', '#F97794', 'FCCF31', '#F761A1', '#43CBFF', '#FFD26F', '#F6CEEC','#97ABFF'];
 
 function setOption(chart, option) {
   chart.setOption(option);
@@ -14,20 +16,24 @@ Page({
     },
     isLoaded: false,
     isDisposed: false,
-    boxInfo:[],
+    boxInfo: [],
   },
-  onLoad: function (options) {
- 
+  onLoad: function(options) {
+    wx.setNavigationBarTitle({
+      title: '实时票房数据'
+    })
   },
-  onReady: function () {
+  onReady: function() {
     // 获取组件
     this.ecComponent = this.selectComponent('#box-pie');
   },
-  init: function () {
+  init: function() {
+    var that = this;
+    var movies = that.data.boxInfo;
     console.log('开始绘制表格');
     const option = {
-      backgroundColor: "#ffffff",
-      color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+      backgroundColor: "#EE9AE5",
+      color: COLORS,
       series: [{
         label: {
           normal: {
@@ -52,8 +58,7 @@ Page({
         }, {
           value: 38,
           name: '上海'
-        },
-        ],
+        }, ],
         itemStyle: {
           emphasis: {
             shadowBlur: 10,
@@ -68,7 +73,7 @@ Page({
         width: width,
         height: height
       });
-      setOption(chart,option);
+      setOption(chart, option);
       this.chart = chart;
       this.setData({
         isLoaded: true,
@@ -77,42 +82,41 @@ Page({
       return chart;
     });
   },
-  onShow: function () {
+  onShow: function() {
     var that = this;
     wx.request({
       url: RANKLIST_URL,
       method: 'GET',
       data: {
-        sortType: '1',
-        date: new Date().getTime()
+        date: new Date().getTime(),
       },
       header: {
         'content-type': 'application/json',
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data.error_code == 0) {
           console.log('调用init方法');
           that.init();
-          that.setDate({
+          that.setData({
             boxInfo: res.data.data
           })
         }
       }
     })
   },
-  onHide: function () {
-  
+  onHide: function() {
+
   },
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
